c9f1fd535c3476a10a48183fc0a29f335c04bd14
Peter Kunszt <pkunszt@gmail.com>
2020-11-25 20:11:08 +0100

    fix: you need to have a local package.json and of course run the npm install for firebase otherwise it is not there to be loaded. Also, the specializations is an ARRAY not just a list of objects, otherwise only the first one gets loaded. Auth is not needed, after all. I also changed how the push is done and added some error logging examples in case the firebase call fails (like perm.denied). Currently our rules are open, anyone can write.

--- upload/import.js
+++ upload/import.js
@@ -1,64 +1,68 @@
 firebase = require('firebase/app');
 require('firebase/database');
-require ('firebase/auth');
+require('firebase/auth');
 
-firebase.initializeApp( {
-    apiKey: 'AIzaSyB07oJq9-o40GunNJy1wrUdWFVZXnok32s',
-    authDomain: 'wellbeing-dev.firebaseapp.com',
-    databaseURL: 'https://wellbeing-dev.firebaseio.com/',
-    projectId: 'wellbeing-dev',
-    storageBucket: 'wellbeing-dev.appspot.com'
-  });
+firebase.initializeApp({
+  apiKey: 'AIzaSyB07oJq9-o40GunNJy1wrUdWFVZXnok32s',
+  authDomain: 'wellbeing-dev.firebaseapp.com',
+  databaseURL: 'https://wellbeing-dev.firebaseio.com/',
+  storageBucket: 'wellbeing-dev.appspot.com'
+});
 var db = firebase.database();
 var specializations = JSON.parse(JSON.stringify(
+  [
     {
-        "name_de":"ONK",
-        "name_en":"ENT"
-    },
+      "name_de": "ONK",
+      "name_en": "ENT"
+    }
+    ,
     {
-        "name_de":"Physiotherapeut",
-        "name_en":"Physiotherapist"
-    },
+      "name_de": "Physiotherapeut",
+      "name_en": "Physiotherapist"
+    }
+    ,
     {
-        "name_de":"Radiologe",
-        "name_en":"Radiologist"
-    },
+      "name_de": "Radiologe",
+      "name_en": "Radiologist"
+    }
+    ,
     {
-        "name_de":"Zahnarzt",
-        "name_en":"Dentist"
-    },
+      "name_de": "Zahnarzt",
+      "name_en": "Dentist"
+    }
+    ,
     {
-        "name_de":"Chirurg",
-        "name_en":"Surgeon"
-    },
+      "name_de": "Chirurg",
+      "name_en": "Surgeon"
+    }
+    ,
     {
-        "name_de":"Arzt",
-        "name_en":"Physician"
-    }));
+      "name_de": "Arzt",
+      "name_en": "Physician"
+    }
+  ]));
 
 
-function writeSpecializations(){
-    console.log("Signing In..");
-    firebase.auth().signInWithEmailAndPassword("meghanavmisal@gmail.com", "meghana7")
-    .then((user) => {
-       console.log(user.email +"signed In");
-       db.ref('/specializations').push(specializations);
-    })
-    .catch((error) => {
-        var errorCode = error.code;
-        console.log("Error code"+ errorCode);
-        var errorMessage = error.message; 
-        console.log("Error Message"+ errorMessage);
+function writeSpecializations() {
+  var ref = db.ref('/specializations');
+  console.log('got ref');
+  var any = ref.push(null, err => {
+    if (err) {
+      console.log(err);
+    }
+  });
+  any.set(specializations, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  }).then()
+    .catch(e => {
+      if (e) {
+        console.log(e);
+      }
     });
+  console.log('write succeeded.');
 }
 
 writeSpecializations();
 
-function logout(){
-    firebase.auth().signOut().then(function() {
-        console.log("Sign-out successful");
-    }).catch(function(error) {
-        // An error happened.
-    });
-}
-logout();

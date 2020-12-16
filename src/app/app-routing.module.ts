53147627ff33e343eb56d016d7ae6321732be14e
Meghna <meghana.misal@swqaindia.com>
2020-12-16 08:43:13 +0530

    route bug fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -39,6 +39,6 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules, useHash: true })
   ],
-  exports: [RouterModule,ProfilePageRoutingModule]
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }

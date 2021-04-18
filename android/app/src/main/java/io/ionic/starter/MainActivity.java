703524c082eb6cfe278d7e64ba52c77be5ea3338
vt90 <tomsavlad90@gmail.com>
2021-04-18 19:49:17 +0300

    wip - prepare app center
    Signed-off-by: vlad.tomsa <vlad.tomsa@persona.im>

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
 
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+
 import java.util.ArrayList;
 
 public class MainActivity extends BridgeActivity {
@@ -17,5 +21,7 @@ public class MainActivity extends BridgeActivity {
       // Additional plugins you've installed go here
       // Ex: add(TotallyAwesomePlugin.class);
     }});
+
+    AppCenter.start(getApplication(), "b44ad3d0-2ee7-4496-8049-59790f379091", Analytics.class, Crashes.class);
   }
 }
